26e2b2a90695c0850dce463e6ccaec5f1e730988
Cosmin <cosminrosu.csr@gmail.com>
2025-04-12 13:13:59 +0300

    version 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import UserGreeting from "./UserGreeting.jsx"
 
 function App() {
   
   return (
     <>
+      <UserGreeting isLoggedIn = {true} username = "Cosmin"/>
+      <UserGreeting isLoggedIn = {true} />
     </>
   )
 }

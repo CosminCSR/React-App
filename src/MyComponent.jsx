2227c3a4be3d13c44d40bc535d3173bbcbf70eb1
Cosmin <cosminrosu.csr@gmail.com>
2025-04-13 20:40:52 +0300

    version 11

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,31 +1,30 @@
 import React, {useState} from 'react'
 
 function MyComponent() {
-  const [count, setCount] = useState(0)
+  const [foods, setFoods] = useState(["apple", "orange", "banana"])
 
-  function increment() {
-    setCount(c  => c + 1)
-    setCount(c  => c + 1)
-    setCount(c  => c + 1)
+  function handleAddFood() {
+    const newFood = document.getElementById("foodInput").value
+    document.getElementById("foodInput").value = ""
+    setFoods(f => [...f, newFood])
   }
-  function decrement() {
-    setCount(c => c - 1)
-    setCount(c => c - 1)
-    setCount(c => c - 1)
-  }
-  function reset() {
-    setCount(0)
+
+  function handleRemoveFood(index) {
+    setFoods(foods.filter((_, i) => i !== index))
   }
 
   return(
-    <>
-    <div>
-      <p>Count: {count}</p>
-      <button onClick = {increment}>Increment</button>
-      <button onClick = {decrement}>Decrement</button>
-      <button onClick = {reset}>Reset</button>
-    </div>
-    </>
+   <div>
+    <h2>List of Food</h2>
+    <ul>
+      {foods.map((food, index) => 
+      <li key = {index} onClick = {() => handleRemoveFood(index)}>
+        {food}
+      </li>)}
+    </ul>
+    <input type = "text" id = "foodInput" placeholder = "enter food name"/>
+    <button onClick = {handleAddFood}>Add Food</button>
+   </div>
   )
 }
 
